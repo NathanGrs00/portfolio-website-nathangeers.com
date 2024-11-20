52680b5a4bd20456ffecc42a5c9bfeac1b5be092
NathanGRSDesktop <nathangeers00@gmail.com>
2024-11-20 13:25:24 +0100

    refactor(last fix)

--- public/javascript/emailjs.js
+++ public/javascript/emailjs.js
@@ -6,14 +6,30 @@
 })();
 
 window.onload = function() {
-// When submitting the form, prevent the default working and sends the form through the template set in emailJS.
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    emailjs.sendForm('default_service', 'contact_form', this, '')
-        .then(() => {
-            alert("Your information has been send. I will get in contact with you shortly.")
-        }, (error) => {
-            console.log('FAILED...', error);
-        });
-});
-}
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+
+        // Get the last submitted time from localStorage (in milliseconds)
+        let lastSubmitTime = localStorage.getItem('lastSubmitTime');
+        let currentTime = new Date().getTime();
+
+        // If the last submit time exists and is less than an hour ago, prevent further submission
+        if (lastSubmitTime && currentTime - lastSubmitTime < 60 * 60 * 1000) {
+            alert('You can only submit the form once per hour.');
+            return; // Prevent form submission
+        }
+
+        // Prevent the form from submitting the default way
+        event.preventDefault();
+
+        // Send the form via emailjs
+        emailjs.sendForm('default_service', 'contact_form', this, '')
+            .then(() => {
+                alert("Your information has been sent. I will get in contact with you shortly.");
+
+                // Save the current time as the last submitted time (only after success)
+                localStorage.setItem('lastSubmitTime', currentTime);
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+};
